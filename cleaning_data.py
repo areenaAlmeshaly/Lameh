a60a7dc0ae69bed5be18744622702ad2b52d38d6
Areena <areena@Areenas-MacBook-Air.local>
2026-08-12 04:16:10 +0300

    Updated Cleaning

--- cleaning_data.py
+++ cleaning_data.py
@@ -7,3 +7,14 @@ def turning_categ(df,review):
                  if dec=="YES" and df[column].dtype != "object":
                       df[column] = df[column].astype("object")
   return df.dtypes
+
+def is_ID(df,review):
+  ID = []
+  for item in review:
+            if item["reason"] == "High unique ratio - possible identifier":
+                 column = item["column"]
+                 dec=input(print("Is",column ,"an ID column ? (YES)(NO) "))
+                 if dec=="YES" and df[column].dtype != "object":
+                      df[column] = df[column].astype("object")
+                      ID.append(column)
+  return ID
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import pandas as pd
 from file_loader import load_file
 from data_overview import data_summary
 from schema_detection import detect_numeric
-from schema_detection import detect_date 
 from schema_detection import classify_columns 
 from cleaning_data import turning_categ
+from cleaning_data import is_ID
+
 file_path = input("Enter your file path: ")
 df=load_file(file_path)
 
@@ -20,3 +21,5 @@ print(classify_columns(df,info))
 review=classify_columns(df,info)
 print("----------------------")
 print(turning_categ(df,review))
+print("----------------------")
+print(is_ID(df,review))
