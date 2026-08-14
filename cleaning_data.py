5f7bfdc93144bf1e94441df7994801b07588beac
Areena <areena@Areenas-MacBook-Air.local>
2026-08-14 04:16:38 +0300

    dealing with duplicates and null values

--- cleaning_data.py
+++ cleaning_data.py
@@ -3,18 +3,51 @@ def turning_categ(df,review):
   for item in review:
             if item["reason"] == "Numeric column with few unique values - may be categorical":
                  column = item["column"]
-                 dec=input(print("Is",column ,"a categorical column ? (YES)(NO) "))
-                 if dec=="YES" and df[column].dtype != "object":
+                 dec=input(f"Is {column} a categorical column ? (yes)(no)")
+                 if dec.lower()=="yes" and df[column].dtype != "object":
                       df[column] = df[column].astype("object")
-  return df.dtypes
+  return df
 
 def is_ID(df,review):
   ID = []
   for item in review:
             if item["reason"] == "High unique ratio - possible identifier":
                  column = item["column"]
-                 dec=input(print("Is",column ,"an ID column ? (YES)(NO) "))
-                 if dec=="YES" and df[column].dtype != "object":
+                 dec=input(f"Is {column} an ID column ? (yes)(no) ")
+                 if dec.lower()=="yes" and df[column].dtype != "object":
                       df[column] = df[column].astype("object")
                       ID.append(column)
   return ID
+
+def not_full_num(df,review):
+     for item in review:
+                  if item["reason"] == "Not fully numeric":
+                       column = item["column"]
+                       converted = pd.to_numeric(df[column], errors="coerce")
+                       invalid_values = df[column][converted.isna() & df[column].notna()]
+                       print(column)
+                       print("Invalid values:")
+                       print(invalid_values)
+                       df[column] = converted
+     return df
+
+def drop_dupl(df):
+    return df.drop_duplicates(keep="first")
+
+def null_val(df,missing_values):
+     deal_cols=[]
+     for col in missing_values.index:
+          perc = missing_values.loc[col, "null perc"]
+
+          if perc>0:
+                if perc>=50:
+                      print(f"WARNING : missing values in {col} To high to drop it ! ")
+
+                dec=input(f"what do you want to do with null values in {col} it has a percent {perc} drop OR Deal with it ?")
+                if dec.lower()=="drop":
+                  df = df.dropna(subset=[col])
+
+                elif dec.lower() == "deal":    
+                            deal_cols.append(col)
+     return deal_cols,df
+                
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #.  /Users/areena/Desktop/HR_Attrition_Project/IBM Data.csv
 #  /Users/areena/Desktop/dirty_cafe_sales.csv
+# /Users/areena/Desktop/marketing_campaign.csv
 import pandas as pd
 from file_loader import load_file
 from data_overview import data_summary
@@ -7,6 +8,8 @@ from schema_detection import detect_numeric
 from schema_detection import classify_columns 
 from cleaning_data import turning_categ
 from cleaning_data import is_ID
+from cleaning_data import not_full_num
+from cleaning_data import null_val
 
 file_path = input("Enter your file path: ")
 df=load_file(file_path)
@@ -19,7 +22,11 @@ info=detect_numeric(df)
 print("----------------------")
 print(classify_columns(df,info))
 review=classify_columns(df,info)
-print("----------------------")
-print(turning_categ(df,review))
+df=turning_categ(df,review)
 print("----------------------")
 print(is_ID(df,review))
+print("----------------------")
+df=not_full_num(df,review)
+print("----------------------")
+deal_cols,df=null_val(df,missing_values)
+print(df.dtypes)
